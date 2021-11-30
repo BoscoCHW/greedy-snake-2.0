7b6ef8ffb47e0909d48b4498670d7aa023aeaf7e
Bosco_Acer <bosco.chw@gmail.com>
2021-11-29 21:41:23 -0800

    finish start and end screen

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import pygame
 import pygame.locals
 from snake import Snake
 from food import Food
-
+import sys
 
 WIDTH, HEIGHT = 400, 400
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Greedy Snake")
+pygame.display.set_caption("Greedy Snake 2.0")
 
 # Colors
 BLACK = (0, 0, 0)
@@ -16,6 +16,7 @@ MODAL_BACKGROUND = (50, 50, 50)
 
 # Text 
 SMALL_FONT_SIZE = 12
+MEDIUM_FONT_SIZE = 20
 BIG_FONT_SIZE = 30
 SNAKE_SCORE_POS = (WIDTH-90, 5)
 PLAYER2_SCORE_POS = (5, 5)
@@ -26,6 +27,10 @@ DURATION = 2 * 60
 
 FPS = 20
 
+def quit_game():
+  pygame.quit()
+  sys.exit()
+
 def render_text_surface(text: str, color: tuple, size: int):
   """Utility function to render a text surface"""
   arial = pygame.font.SysFont("arial", size)
@@ -33,12 +38,26 @@ def render_text_surface(text: str, color: tuple, size: int):
 
   return text_surface
 
+def pos_x_for_centering(bg: pygame.Surface, fg: pygame.Surface):
+  return bg.get_rect().centerx - fg.get_rect().width / 2
+
+def draw_start():
+  WINDOW.fill(BLACK)
+  greeting_surf = render_text_surface("Welcome to Greedy Snake 2.0", WHITE, MEDIUM_FONT_SIZE)
+  greeting_pos_x = pos_x_for_centering(WINDOW, greeting_surf)
+  WINDOW.blit(greeting_surf, (greeting_pos_x, 100))
+
+  instr_surf = render_text_surface("Press any key to start.", WHITE, SMALL_FONT_SIZE)
+  instr_pos_x = pos_x_for_centering(WINDOW, instr_surf)
+  WINDOW.blit(instr_surf, (instr_pos_x, 200))
+  pygame.display.update()
+
 def draw_window(snake: Snake, food: Food, snake_score: int, player2_score: int, time_remained: int):
   """ draw the window at the end of every loop"""
   WINDOW.fill(BLACK)
 
   time_surf = render_text_surface(f"Remaining time: {time_remained}s", WHITE, SMALL_FONT_SIZE)
-  time_pos_x = WINDOW.get_rect().centerx - time_surf.get_rect().width / 2
+  time_pos_x = pos_x_for_centering(WINDOW, time_surf)
   WINDOW.blit(time_surf, (time_pos_x, 5))
 
   snake_score_surf = render_text_surface(f"Snake Score: {snake_score}", WHITE, SMALL_FONT_SIZE)
@@ -55,18 +74,28 @@ def draw_window(snake: Snake, food: Food, snake_score: int, player2_score: int, 
 
 def draw_end(snake_score, player2_score, snake):
   """ draw the end game window """
-  modal_bg = pygame.draw.rect(WINDOW, MODAL_BACKGROUND, WINDOW.get_rect().inflate(-200, -300))
+  modal_surf = pygame.Surface((300, 120))
+  modal_surf.fill(MODAL_BACKGROUND)
+
   if snake.state == "DEAD":
-    modal_text = render_text_surface("Snake Lost!", WHITE, BIG_FONT_SIZE)
+    verdict_surf = render_text_surface("Snake Lost!", WHITE, BIG_FONT_SIZE)
   else:
     if snake_score > player2_score:
-      modal_text = render_text_surface("Snake Won!", WHITE, BIG_FONT_SIZE)
+      verdict_surf = render_text_surface("Snake Won!", WHITE, BIG_FONT_SIZE)
     else:
-      modal_text = render_text_surface("Snake Lost!", WHITE, BIG_FONT_SIZE)
+      verdict_surf = render_text_surface("Snake Lost!", WHITE, BIG_FONT_SIZE)
+
+  instr_surf = render_text_surface("Press any key to restart or Esc to exit.", WHITE, SMALL_FONT_SIZE)
+
+  verdict_pos_x = pos_x_for_centering(modal_surf, verdict_surf)
+  instr_pos_x = pos_x_for_centering(modal_surf, instr_surf)
+
+  modal_surf.blit(verdict_surf, (verdict_pos_x, 20))
+  modal_surf.blit(instr_surf, (instr_pos_x, 80))
+
   
-  modal_text_width, modal_text_height = modal_text.get_rect().width, modal_text.get_rect().height
-  modal_text_pos = (modal_bg.centerx - modal_text_width / 2, modal_bg.centery - modal_text_height / 2)
-  WINDOW.blit(modal_text, modal_text_pos)
+  WINDOW.blit(modal_surf.convert(), (50, 100))
+
   pygame.display.update()
 
 def create_food(snake: Snake):
@@ -112,64 +141,77 @@ def main():
   snake_score = 0
   food = create_food(snake)
   player2_score = 0
+  game_state = "READY"
 
   running = True
   while running:
-
+    
     clock.tick(FPS)
-
-    time_in_sec = int(pygame.time.get_ticks() / 1000)
-    time_remaining = DURATION - time_in_sec  # calculate remaining time
-
     for event in pygame.event.get():
-      # quit the game if the close window button is clicked
+      # quit the game if the close window button is clicked or Esc is pressed
       if event.type == pygame.locals.QUIT:
-        running = False
-        pygame.quit()
-
+        quit_game()
+      elif event.type == pygame.KEYDOWN:
+        if event.key == 27:   # Esc is pressed
+          quit_game()
 
     keys = pygame.key.get_pressed()
 
-    # when key is pressed, change direction
-    if keys[pygame.locals.K_UP] and snake.direction != "DOWN":
-      snake.direction = "UP"
-    elif keys[pygame.locals.K_DOWN] and snake.direction != "UP":
-      snake.direction = "DOWN"
-    elif keys[pygame.locals.K_LEFT] and snake.direction != "RIGHT":
-      snake.direction = "LEFT"
-    elif keys[pygame.locals.K_RIGHT] and snake.direction != "LEFT":
-      snake.direction = "RIGHT"
-    
-    # move the snake
-    snake.move()
+    if game_state == "READY":
+      draw_start()
+      if any(keys):
+        game_state = "RUNNING"
+        
+    elif game_state == "RUNNING":
+      time_in_sec = int(pygame.time.get_ticks() / 1000)
+      time_remaining = DURATION - time_in_sec  # calculate remaining time
 
-    # when the snake eats the food
-    if snake.head.rect.colliderect(food.rect):
-      # make a new food
-      food = create_food(snake)
+      # when key is pressed, change direction
+      if keys[pygame.locals.K_UP] and snake.direction != "DOWN":
+        snake.direction = "UP"
+      elif keys[pygame.locals.K_DOWN] and snake.direction != "UP":
+        snake.direction = "DOWN"
+      elif keys[pygame.locals.K_LEFT] and snake.direction != "RIGHT":
+        snake.direction = "LEFT"
+      elif keys[pygame.locals.K_RIGHT] and snake.direction != "LEFT":
+        snake.direction = "RIGHT"
+      
+      # move the snake
+      snake.move()
 
-      # only neutralize the growing when the second player pressed
-      # a key that is opposite to the snake's direction
-      if check_neutralize(keys, snake):
-        player2_score += 2
+      # when the snake eats the food
+      if snake.head.rect.colliderect(food.rect):
+        # make a new food
+        food = create_food(snake)
 
-      else:
-        # the second player fails to press the corresponding key
-        # the snake grows
-        snake.grow()
-        snake_score += 1
+        # only neutralize the growing when the second player pressed
+        # a key that is opposite to the snake's direction
+        if check_neutralize(keys, snake):
+          player2_score += 2
 
-    
-    # if the snake eats its body or hit the window, game ends 
-    check_snake(snake)
-    if snake.state == "DEAD" or time_remaining <= 0:
-      break
-    
-    draw_window(snake, food, snake_score, player2_score, time_remaining)
+        else:
+          # the second player fails to press the corresponding key
+          # the snake grows
+          snake.grow()
+          snake_score += 1
 
-  # when game ends, draw the end game modal
-  draw_end(snake_score, player2_score, snake)
-  pygame.time.wait(4000)
+      
+      # if the snake eats its body or hit the window, game ends 
+      check_snake(snake)
+      if snake.state == "DEAD" or time_remaining <= 0:
+        game_state = "END"
+      
+      draw_window(snake, food, snake_score, player2_score, time_remaining)
+
+    elif game_state == "END":
+      # when game ends, draw the end game modal
+      draw_end(snake_score, player2_score, snake)
+      if any(keys):
+        snake = Snake()
+        snake_score = 0
+        food = create_food(snake)
+        player2_score = 0
+        game_state = "RUNNING"
 
 if __name__ == "__main__":
   main()
